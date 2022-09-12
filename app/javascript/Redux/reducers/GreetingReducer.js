9043533d11ecf8da053aa9fbbeffa930be78560d
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-12 16:11:45 +0300

    Add reducer function

---
+++ app/javascript/Redux/reducers/GreetingReducer.js
@@ -0,0 +1,26 @@
+import Axios from 'axios';
+
+const SETGREETINGS = 'react-app/redux/SETGREETINGS';
+
+const setGreetings = (greetings) => ({
+    type: SETGREETINGS,
+    greetings
+});
+
+const initialState = [];
+
+export const getGreetings = () => async (dispatch) => {
+    const { data } = await Axios.get('/greetings');
+    dispatch(setGreetings(data));
+}
+
+const greetingReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SETGREETINGS:
+            return action.greetings;
+        default:
+            return state;
+    }
+}
+
+export default greetingReducer;
